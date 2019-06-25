e9bd88afb9913f29882382b5742535b43658d0f5
Akash Shaw <akashshaw@shaw.local>
2019-06-25 08:33:37 +0530

    updating the client bundle to hydrate contents into browser

--- src/client/client.js
+++ src/client/client.js
@@ -1,2 +1,7 @@
 // root for the client side code bases
 
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Home from './components/Home'
+
+ReactDOM.hydrate(<Home/>, document.querySelector('#root'))
--- src/index.js
+++ src/index.js
@@ -13,11 +13,26 @@ import Home from './client/components/Home'
 
 const app = express()
 
+// tell express to freely serve the public folder, to treat it as static
+app.use(express.static('public'))
+
 
 app.get('/', (req, res) => {
     const content = renderToString(<Home/>)
 
-    res.send(content)
+    const html = `
+        <html>
+            <head></head>
+            <body>
+                <div id="root">
+                    ${content}
+                </div>
+                <script src="bundle.js"></script>
+            </body>
+        </html>
+    `
+
+    res.send(html)
 })
 
 app.listen(3000, ()=>{
