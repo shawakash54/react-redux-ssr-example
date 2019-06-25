14507179c23a179dfac6e7cd16b7c0b4458cf669
Akash Shaw <akashshaw@shaw.local>
2019-06-25 07:33:59 +0530

    server side node js code converted to es2015

--- src/index.js
+++ src/index.js
@@ -1,15 +1,22 @@
 // root file for the application in server
 // here we are using commonjs syntax
 
-const express = require('express')
-const React = require('react')
-const renderToString = require('react-dom/server').renderToString
-const Home = require('./client/components/Home').default
+// const express = require('express')
+// const React = require('react')
+// const renderToString = require('react-dom/server').renderToString
+// const Home = require('./client/components/Home').default
+
+import express from 'express'
+import React from 'react'
+import { renderToString } from 'react-dom/server'
+import Home from './client/components/Home'
+
 const app = express()
+
 
 app.get('/', (req, res) => {
     const content = renderToString(<Home/>)
-    
+
     res.send(content)
 })
 
